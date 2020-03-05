f1db183a23ea92d78aec66074f371ef4d67c6cdb
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-03-05 10:24:48 -0500

    Añadido Material-UI con hack add: Libreria Material-UI

--- app.jsx
+++ app.jsx
@@ -1,11 +1,15 @@
 import React from "React";
 import ReactDOMServer from "ReactDOMServer";
+import { Button } from "@material-ui/core";
 import { serve } from "deno_http/server.ts";
 
 const App = (a) => {
   return (
     <React.Fragment>
       <h1>React App with Deno</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </React.Fragment>
   );
 };
