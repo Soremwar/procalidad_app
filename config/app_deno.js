c218dd39864b50e617be36eae6894440677cbedf
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-07-30 17:29:06 -0500

    Arreglar configuracion de app para Deno

--- config/app_deno.js
+++ config/app_deno.js
@@ -7,8 +7,8 @@ import {
 
 const config = await readJson("config.json");
 
-const address = config?.app?.address || window.location.hostname;
-const port = Number(config?.app?.port) || window.location.port;
+const address = config?.app?.address || "127.0.0.1";
+const port = Number(config?.app?.port) || "80";
 const protocol = config?.app?.protocol || "http";
 const version = config?.app?.version || "NA";
 
