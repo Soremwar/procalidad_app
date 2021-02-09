b551af215041260b60f8e2aa47cb27d2b7de2f4d
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-02-09 14:17:32 -0500

    fix: Gantt no funciona al compilar en modo produccion

--- components/pages/planeacion/recurso.jsx
+++ components/pages/planeacion/recurso.jsx
@@ -21,7 +21,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { FrappeGantt, Task, ViewMode } from "frappe-gantt-react";
+import { FrappeGantt, Task, ViewMode } from "react-frappe-gantt";
 import { formatResponseJson } from "../../../lib/api/request.ts";
 import {
   formatStandardNumberToStandardString,
